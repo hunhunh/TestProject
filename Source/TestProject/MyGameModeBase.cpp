fc37d54b8554a9ab0cb46b51124e1ceafe9e6ccf
hunhunh <chikcop@naver.com>
2024-01-16 21:40:36 +0900

    Create AnimInstance Cpp

--- Source/TestProject/MyGameModeBase.cpp
+++ Source/TestProject/MyGameModeBase.cpp
@@ -6,7 +6,7 @@
 
 AMyGameModeBase::AMyGameModeBase()
 {
-	static ConstructorHelpers::FClassFinder<ACharacter> DefaultPawnClassRef(TEXT("/Script/Engine.Blueprint'/Game/BP_MyCharacter.BP_MyCharacter_C'"));
+	static ConstructorHelpers::FClassFinder<ACharacter> DefaultPawnClassRef(TEXT("/Script/Engine.Blueprint'/Game/BluePrint/BP_MyCharacter.BP_MyCharacter_C'"));
 	if (DefaultPawnClassRef.Class)
 	{
 		DefaultPawnClass = DefaultPawnClassRef.Class;
