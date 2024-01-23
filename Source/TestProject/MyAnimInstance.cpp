9ec3e1aebea5f1a8f5d57ee6321c0b2f65f216a6
hunhunh <chikcop@naver.com>
2024-01-23 09:23:34 +0900

    Update

--- Source/TestProject/MyAnimInstance.cpp
+++ Source/TestProject/MyAnimInstance.cpp
@@ -59,7 +59,59 @@ void UMyAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
 		FRotator DeltaRotation = VelocityRotation - AimRotation;
 		DeltaRotation.Normalize();
 		YawOffset = DeltaRotation.Yaw;
+
+		if (bShouldMove || bIsFalling)
+		{
+			RotateYaw = FMath::FInterpTo(RotateYaw, 0.f, DeltaSeconds, 20.0f);
+			MovingRotation = MyCharacter->GetActorRotation();
+			PrevRotation = MovingRotation;
+
+		}
+		else
+		{
+			PrevRotation = MovingRotation;
+			MovingRotation = MyCharacter->GetActorRotation();
+			FRotator Delta = MovingRotation - PrevRotation;
+			Delta.Normalize();
+			RotateYaw -= Delta.Yaw;
+
+			float TurnValue = GetCurveValue("Turn");
+			if (TurnValue > 0.f)
+			{
+				PrevDistanceCurve = DistanceCurve;
+				DistanceCurve = GetCurveValue("DistanceCurve");
+				DeltaDistanceCurve = DistanceCurve - PrevDistanceCurve;
+				if (RotateYaw > 0.f)
+				{
+					RotateYaw -= DeltaDistanceCurve;
+				}
+				else
+				{
+					RotateYaw += DeltaDistanceCurve;
+				}
+
+				float AbsRotateYawOffset = FMath::Abs(RotateYaw);
+				if (AbsRotateYawOffset > 0.f)
+				{
+					float YawExcess = AbsRotateYawOffset - 90.f;
+					if (RotateYaw > 0.f)
+					{
+						RotateYaw -= YawExcess;
+					}
+					else
+					{
+						RotateYaw += YawExcess;
+					}
+
+				}
+			}
+
+		}
+
+
+
 	}
+	
 }
 
 void UMyAnimInstance::PlayFireMontage()
--- Source/TestProject/MyAnimInstance.h
+++ Source/TestProject/MyAnimInstance.h
@@ -41,6 +41,22 @@ private:
 
 	UPROPERTY(Category = "Animation", EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = true))
 	float RotateYaw;
+
+	UPROPERTY(Category = "Animation", EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = true))
+	FRotator MovingRotation;
+
+	UPROPERTY(Category = "Animation", EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = true))
+	FRotator PrevRotation;
+
+	UPROPERTY(Category = "Animation", EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = true))
+	float DistanceCurve;
+
+	UPROPERTY(Category = "Animation", EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = true))
+	float PrevDistanceCurve;
+
+	UPROPERTY(Category = "Animation", EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = true))
+	float DeltaDistanceCurve;
+
 public:
 	UMyAnimInstance();
 	virtual void NativeInitializeAnimation() override;
